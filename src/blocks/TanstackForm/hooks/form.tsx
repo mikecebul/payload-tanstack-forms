b541ffa79458e608e0c43790e933fff49823a66c
mikecebul <mikecebul@gmail.com>
2025-03-18 14:31:59 -0400

    Organize components, use form state instead of Subscribe component

--- src/blocks/TanstackForm/hooks/form.tsx
+++ src/blocks/TanstackForm/hooks/form.tsx
@@ -1,29 +1,14 @@
 'use client'
 
 import { createFormHook } from '@tanstack/react-form'
-import { fieldContext, formContext, useFormContext } from './form-context'
-import { Button } from '@/components/ui/button'
-import { Loader } from 'lucide-react'
+import { fieldContext, formContext } from './form-context'
 import TextareaField from '../field-components/textarea-field'
 import TextField from '../field-components/text-field'
 import CheckboxField from '../field-components/checkbox-field'
 import NumberField from '../field-components/number-field'
-import { ArrayFieldComponent } from '../form-components/array-field'
 import EmailField from '../field-components/email-field'
 import PhoneField from '../field-components/phone-field'
-
-function SubscribeButton({ label }: { label: string }) {
-  const form = useFormContext()
-  return (
-    <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
-      {([canSubmit, isSubmitting]) => (
-        <Button className="w-full" disabled={!canSubmit || isSubmitting}>
-          {isSubmitting ? <Loader className="animate-spin" /> : label}
-        </Button>
-      )}
-    </form.Subscribe>
-  )
-}
+import SubscribeButton from '../form-components/subscribe-button'
 
 export const { useAppForm, withForm } = createFormHook({
   fieldComponents: {
@@ -36,7 +21,6 @@ export const { useAppForm, withForm } = createFormHook({
   },
   formComponents: {
     SubscribeButton,
-    ArrayFieldComponent,
   },
   fieldContext,
   formContext,
--- src/blocks/TanstackForm/tanstack-form-block.tsx
+++ src/blocks/TanstackForm/tanstack-form-block.tsx
@@ -8,8 +8,9 @@ import { getClientSideURL } from '@/utilities/getURL'
 import { useRouter } from 'next/navigation'
 import RichText from '@/components/RichText'
 import { z } from 'zod'
-import { ArrayFieldComponent } from './form-components/array-field'
+import { ArrayFieldComponent } from './withForm-components/array-field'
 import { getDefaultValuesOpts } from './form-options'
+import { useStore } from '@tanstack/react-form'
 
 export const TanstackFormBlock = ({
   form: formFromProps,
@@ -26,7 +27,6 @@ export const TanstackFormBlock = ({
   } = typeof formFromProps !== 'string' ? formFromProps : {}
   const [postError, setPostError] = useState<{ message: string; status?: string } | undefined>()
   const router = useRouter()
-  // const defaultValues = useDefaultValues()
 
   const form = useAppForm({
     ...getDefaultValuesOpts(fields),
@@ -77,186 +77,183 @@ export const TanstackFormBlock = ({
       }
     },
   })
+  const [isSubmitSuccessful] = useStore(form.store, (state) => [state.isSubmitSuccessful])
 
   return (
     <div className="max-w-2xl mx-auto">
       {enableIntro && introContent && (
         <RichText className="mb-8 lg:mb-12" data={introContent} enableGutter={false} />
       )}
-      <form.Subscribe selector={(state) => [state.isSubmitSuccessful]}>
-        {([isSubmitSuccessful]) =>
-          isSubmitSuccessful && confirmationType === 'message' && confirmationMessage ? (
-            <RichText data={confirmationMessage} />
-          ) : (
-            <>
-              {postError && <div>{`${postError.status || '500'}: ${postError.message || ''}`}</div>}
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault()
-                  e.stopPropagation()
-                  form.handleSubmit()
-                }}
-              >
-                <Card className="@container">
-                  <CardContent className="grid grid-cols-1 gap-4 @lg:grid-cols-2 p-6 auto-cols-fr">
-                    {fields &&
-                      fields?.map((field) => {
-                        switch (field.blockType) {
-                          case 'message':
-                            return null
-                          case 'text':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: field.required
-                                    ? z.string().min(1, `${field.label || field.name} is required`)
-                                    : z.string().optional(),
-                                }}
-                              >
-                                {(formField) => <formField.TextField {...field} />}
-                              </form.AppField>
-                            )
-                          case 'email':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: field.required
-                                    ? z.string().min(1, 'Email is required').email()
-                                    : z.string().email().optional(),
-                                }}
-                              >
-                                {(formField) => <formField.EmailField {...field} />}
-                              </form.AppField>
-                            )
-                          case 'phone':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: field.required
-                                    ? z
-                                        .string()
-                                        .min(1, 'Phone number required')
-                                        .regex(
-                                          /^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-                                          'Invalid phone number',
-                                        )
-                                    : z
-                                        .string()
-                                        .regex(
-                                          /^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-                                          'Invalid phone number',
-                                        )
-                                        .optional(),
-                                }}
-                              >
-                                {(formField) => <formField.PhoneField {...field} />}
-                              </form.AppField>
-                            )
-                          case 'textarea':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: field.required
-                                    ? z.string().min(1, `${field.label || field.name} is required`)
-                                    : z.string().optional(),
-                                }}
-                              >
-                                {(formField) => (
-                                  <formField.TextareaField
-                                    label={field.label ?? field.name}
-                                    width={field.width ?? 100}
-                                    id={field.id ?? field.name}
-                                  />
-                                )}
-                              </form.AppField>
-                            )
-                          case 'checkbox':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: ({ value }) =>
-                                    field.required && Boolean(value) === false
-                                      ? field.errorMsg?.length
-                                        ? field.errorMsg
-                                        : 'Must check to continue'
-                                      : undefined,
-                                }}
-                              >
-                                {(formField) => (
-                                  <formField.CheckboxField
-                                    label={field.label ?? field.name}
-                                    width={field.width ?? 100}
-                                    id={field.id ?? field.name}
-                                  />
-                                )}
-                              </form.AppField>
-                            )
-                          case 'number':
-                            return (
-                              <form.AppField
-                                key={field.id}
-                                name={field.name}
-                                validators={{
-                                  onChange: field.required
-                                    ? z.coerce
-                                        .number()
-                                        .positive(
-                                          `${field.label || field.name} must be a positive number`,
-                                        )
-                                    : z.coerce
-                                        .number()
-                                        .positive(
-                                          `${field.label || field.name} must be a positive number`,
-                                        )
-                                        .or(z.literal(undefined))
-                                        .optional(),
-                                }}
-                              >
-                                {(formField) => (
-                                  <formField.NumberField
-                                    label={field.label ?? field.name}
-                                    width={field.width ?? 100}
-                                    id={field.id ?? field.name}
-                                  />
-                                )}
-                              </form.AppField>
-                            )
-                          case 'array':
-                            return (
-                              <ArrayFieldComponent
-                                key={field.id}
-                                form={form}
-                                field={field}
-                                fields={fields}
+      {isSubmitSuccessful && !postError && confirmationType === 'message' && confirmationMessage ? (
+        <RichText data={confirmationMessage} />
+      ) : (
+        <>
+          {postError && <div>{`${postError.status || '500'}: ${postError.message || ''}`}</div>}
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              form.handleSubmit()
+            }}
+          >
+            <Card className="@container">
+              <CardContent className="grid grid-cols-1 gap-4 @lg:grid-cols-2 p-6 auto-cols-fr">
+                {fields &&
+                  fields?.map((field) => {
+                    switch (field.blockType) {
+                      case 'message':
+                        return null
+                      case 'text':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: field.required
+                                ? z.string().min(1, `${field.label || field.name} is required`)
+                                : z.string().optional(),
+                            }}
+                          >
+                            {(formField) => <formField.TextField {...field} />}
+                          </form.AppField>
+                        )
+                      case 'email':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: field.required
+                                ? z.string().min(1, 'Email is required').email()
+                                : z.string().email().optional(),
+                            }}
+                          >
+                            {(formField) => <formField.EmailField {...field} />}
+                          </form.AppField>
+                        )
+                      case 'phone':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: field.required
+                                ? z
+                                    .string()
+                                    .min(1, 'Phone number required')
+                                    .regex(
+                                      /^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+                                      'Invalid phone number',
+                                    )
+                                : z
+                                    .string()
+                                    .regex(
+                                      /^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+                                      'Invalid phone number',
+                                    )
+                                    .optional(),
+                            }}
+                          >
+                            {(formField) => <formField.PhoneField {...field} />}
+                          </form.AppField>
+                        )
+                      case 'textarea':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: field.required
+                                ? z.string().min(1, `${field.label || field.name} is required`)
+                                : z.string().optional(),
+                            }}
+                          >
+                            {(formField) => (
+                              <formField.TextareaField
+                                label={field.label ?? field.name}
+                                width={field.width ?? 100}
+                                id={field.id ?? field.name}
                               />
-                            )
-                          default:
-                            break
-                        }
-                      })}
-                    {/* <ExampleFieldComponent key={field.id} field={field} form={form} fields={fields} /> */}
-                  </CardContent>
-                  <CardFooter>
-                    <form.AppForm>
-                      <form.SubscribeButton label={submitButtonLabel ?? 'Submit'} />
-                    </form.AppForm>
-                  </CardFooter>
-                </Card>
-              </form>
-            </>
-          )
-        }
-      </form.Subscribe>
+                            )}
+                          </form.AppField>
+                        )
+                      case 'checkbox':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: ({ value }) =>
+                                field.required && Boolean(value) === false
+                                  ? field.errorMsg?.length
+                                    ? field.errorMsg
+                                    : 'Must check to continue'
+                                  : undefined,
+                            }}
+                          >
+                            {(formField) => (
+                              <formField.CheckboxField
+                                label={field.label ?? field.name}
+                                width={field.width ?? 100}
+                                id={field.id ?? field.name}
+                              />
+                            )}
+                          </form.AppField>
+                        )
+                      case 'number':
+                        return (
+                          <form.AppField
+                            key={field.id}
+                            name={field.name}
+                            validators={{
+                              onChange: field.required
+                                ? z.coerce
+                                    .number()
+                                    .positive(
+                                      `${field.label || field.name} must be a positive number`,
+                                    )
+                                : z.coerce
+                                    .number()
+                                    .positive(
+                                      `${field.label || field.name} must be a positive number`,
+                                    )
+                                    .or(z.literal(undefined))
+                                    .optional(),
+                            }}
+                          >
+                            {(formField) => (
+                              <formField.NumberField
+                                label={field.label ?? field.name}
+                                width={field.width ?? 100}
+                                id={field.id ?? field.name}
+                              />
+                            )}
+                          </form.AppField>
+                        )
+                      case 'array':
+                        return (
+                          <ArrayFieldComponent
+                            key={field.id}
+                            form={form}
+                            field={field}
+                            fields={fields}
+                          />
+                        )
+                      default:
+                        break
+                    }
+                  })}
+                {/* <ExampleFieldComponent key={field.id} field={field} form={form} fields={fields} /> */}
+              </CardContent>
+              <CardFooter>
+                <form.AppForm>
+                  <form.SubscribeButton label={submitButtonLabel ?? 'Submit'} />
+                </form.AppForm>
+              </CardFooter>
+            </Card>
+          </form>
+        </>
+      )}
     </div>
   )
 }
